a475b514979357de842271e71ffe91a80c15fdff
Mars <74789790+TypicalMars@users.noreply.github.com>
2024-01-13 15:08:11 -0500

    added wpilib drivetrain libraries

---
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.constants;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FlyingToasters2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "c6f41482a2432151acc89fa6571e7c546a5fa92f";
+  public static final String GIT_DATE = "2024-01-13 13:36:02 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-13 14:10:55 EST";
+  public static final long BUILD_UNIX_TIME = 1705173055638L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
