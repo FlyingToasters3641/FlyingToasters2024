cb504418560cc08186851cb0ffd4d1326195076f
lucas burkowski <lucasburkowski@gmail.com>
2024-01-23 15:45:05 -0500

    Vendor Update

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FlyingToasters2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 17;
-  public static final String GIT_SHA = "717bcaa71f5020cd99b79d4b5d8753f0f107bb11";
-  public static final String GIT_DATE = "2024-01-18 20:58:23 EST";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "970ea2ad6927971d24cf8685354143f57dd2f14c";
+  public static final String GIT_DATE = "2024-01-23 15:37:57 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-23 15:35:00 EST";
-  public static final long BUILD_UNIX_TIME = 1706042100665L;
+  public static final String BUILD_DATE = "2024-01-23 15:44:27 EST";
+  public static final long BUILD_UNIX_TIME = 1706042667659L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
