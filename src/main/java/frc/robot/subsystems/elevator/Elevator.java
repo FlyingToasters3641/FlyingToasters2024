67093038714e228ef2d20f61e9ef6a2e31f42d5d
ArnoldPalmergolf <aadithuchil@gmail.com>
2024-01-31 21:04:18 -0500

    janked up follow command

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,29 +1,36 @@
 package frc.robot.subsystems.elevator;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.subsystems.elevator.*;
 
 public class Elevator extends SubsystemBase {
 
-  public Elevator(ElevatorIO io) 
-  {
-    this.io = io;
-    io.setBrakeMode(false,false);
-  }
   private ElevatorIO io;
   private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
 
-  public Command exampleMethodCommand() {
+  private TalonFX leaderMotor = ElevatorIOTalonFX.leaderTalonFX;
+  private TalonFX followerMotor = ElevatorIOTalonFX.followTalonFX;
 
-    return runOnce(
-        () -> {
 
-        });
+  
+
+  public Elevator(ElevatorIO io) {
+    this.io = io;
+    io.setBrakeMode(false, false);
+
+    followerMotor.setControl(ControlRequest.Follower(leaderMotor));
+    followerMotor.setInverted(InvertType.FollowMaster);
   }
 
-  public boolean exampleCondition() {
-    return false;
-  }
+
 
   @Override
   public void periodic() {
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.elevator;
 
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ElevatorIOTalonFX implements ElevatorIO {
-    private final String CANbusName = "Lucas";
-    private final TalonFX leaderTalonFX = new TalonFX(1, CANbusName);
-    private final TalonFX followTalonFX = new TalonFX(2, CANbusName);
+    private static final String CANbusName = "Lucas";
+    public static final TalonFX leaderTalonFX = new TalonFX(1, CANbusName);
+    public static final TalonFX followTalonFX = new TalonFX(2, CANbusName);
+
+    public static final Follower
 
     public ElevatorIOTalonFX() {
 
