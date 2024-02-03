b2cc82fabb5cda85a4ee402ebe44d8258c814ebf
flyingorange3641 <flyingtoasterorange@gmail.com>
2024-02-03 16:41:30 -0500

    commands and button bunding stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -136,11 +136,13 @@ public class RobotContainer {
                             new Pose2d(m_robotDrive.getPose().getTranslation(), new Rotation2d())),
                     m_robotDrive)
                 .ignoringDisable(true));
-    m_driverController.rightTrigger().whileTrue(IntakeCommands.runFrontSpeed(m_intake, () -> m_driverController.getRightTriggerAxis()));
-
+                 m_driverController.rightTrigger().whileTrue(IntakeCommands.runFrontSpeed(m_intake, () -> m_driverController.getRightTriggerAxis()));
+                 m_driverController.leftTrigger().whileTrue(IntakeCommands.reverseFrontSpeed(m_intake, () -> m_driverController.getRightTriggerAxis()));            
+                 m_driverController.rightBumper().whileTrue(IntakeCommands.runRearSpeed(m_intake, () -> m_driverController.getRightTriggerAxis()));
+                 m_driverController.leftBumper().whileTrue(IntakeCommands.reverseRearSpeed(m_intake, () -> m_driverController.getRightTriggerAxis()));            
+   
   }
-            
-    
+     
   public Command getAutonomousCommand() {
         
      PathPlannerPath path = PathPlannerPath.fromPathFile("TestPath");
--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -1,30 +1,26 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.intake.Intake;
-import java.util.function.DoubleSupplier;
 
 public class IntakeCommands {
 
     private IntakeCommands() {}
 
-    public static Command startFrontRollers(Intake m_intake) {
-        return Commands.run(() -> 
-           {m_intake.startFrontRollers();}
-        );}
-    
-    public static Command stopFrontRollers(Intake m_intake) {
-        return Commands.run(() -> 
-            {m_intake.stopFrontRollers();}
-            );}
-            
-    public static Command reverseFrontRollers(Intake m_intake) {
-        return Commands.run(() -> 
-            {m_intake.reverseFrontRollers();}
-            );}
-    
-    public static Command runFrontSpeed(Intake m_intake, DoubleSupplier axis) {
+    public static Command runFrontSpeed(Intake m_intake, DoubleSupplier axis){
         return Commands.run(() -> {m_intake.runFrontRollers(axis);});
     }
+      public static Command reverseFrontSpeed(Intake m_intake, DoubleSupplier axis){
+        return Commands.run(() -> {m_intake.reverseFrontRollers(axis);});
+    }
+       public static Command runRearSpeed(Intake m_intake, DoubleSupplier axis){
+        return Commands.run(() -> {m_intake.runRearRollers(axis);});
+    }
+      public static Command reverseRearSpeed(Intake m_intake, DoubleSupplier axis){
+        return Commands.run(() -> {m_intake.reverseRearRollers(axis);});
+    }
+    
 }
